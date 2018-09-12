a04ef24225be852119230548eaec07732458e9b5
Junior Oliveira <junior.getsoftware@gmail.com>
2018-09-11 22:24:27 -0300

    :o

--- same/src/main/java/br/com/same/security/MyEncoder.java
+++
@@ -1,9 +0,0 @@
-package br.com.same.security;
-
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-
-public class MyEncoder extends Md5PasswordEncoder {
-	public String encodePassword(String rawPass, Object salt) {
-		return super.encodePassword(rawPass, salt);
-	}
-}
