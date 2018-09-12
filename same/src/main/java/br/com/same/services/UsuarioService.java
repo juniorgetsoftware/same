d9d39b3a9a4f15fbf5426cf09211aa5c770b5c5d
Junior Oliveira <junior.getsoftware@gmail.com>
2018-09-11 22:42:24 -0300

    :(

--- same/src/main/java/br/com/same/services/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package br.com.same.services;
-
-import br.com.same.models.Usuario;
-
-public interface UsuarioService {
-
-	public Usuario usuarioPorEmail(String email);
-
-}
--- same/src/test/java/br/com/same/util/DriverStrategy.java
+++
@@ -1,7 +0,0 @@
-package br.com.same.util;
-
-import org.openqa.selenium.WebDriver;
-
-public interface DriverStrategy {
-	WebDriver getDriver();
-}
